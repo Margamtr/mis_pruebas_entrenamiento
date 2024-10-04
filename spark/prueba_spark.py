d16994cb22ea65a48b0248c2ff9b7f144275d5e6
Margarita Torres <163369688+Margamtr@users.noreply.github.com>
2024-10-04 18:15:18 +0200

    pruebas Spark

---
+++ spark/prueba_spark.py
@@ -0,0 +1,38 @@
+
+import findspark
+from pyspark.sql import SparkSession 
+findspark.init()
+spark = SparkSession.builder.getOrCreate()
+spark
+
+df = spark.createDataFrame(
+    [
+        ("sue", 32),
+        ("li", 3),
+        ("bob", 75),
+        ("heo", 13),
+    ],
+    ["first_name", "age"],
+)
+df = spark.createDataFrame(
+    [
+        ("sue", 32),
+        ("li", 3),
+        ("bob", 75),
+        ("heo", 13),
+    ],
+    ["first_name", "age"],
+)
+
+df.show()
+
+from pyspark.sql.functions import col, when
+
+df1 = df.withColumn(
+    "life_stage",
+    when(col("age") < 13, "child")
+    .when(col("age").between(13, 19), "teenager")
+    .otherwise("adult"),
+)
+df1.show()
+
